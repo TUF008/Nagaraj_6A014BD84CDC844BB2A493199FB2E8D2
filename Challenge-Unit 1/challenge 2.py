901c956e80f428a6202ad9fe4b230cbd55a5d28c
TUF008 <144257444+TUF008@users.noreply.github.com>
2023-09-13 20:24:19 +0530

    challenge2.py

--- Challenge-Unit 1/challenge 2.py
+++ Challenge-Unit 1/challenge 2.py
@@ -1,6 +1,14 @@
 #Leap year
-year=int(input("In enter the year:"))
-if(year % 4==0 and year % 100!=0 or year % 400==0):
-    print("The year is a leap year!")
+def isLeapYear(year):
+  if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    return True
+  else:
+    return False
+
+year = int (input("In enter a year : "))
+
+if isLeapYear(year):
+  print('{} is a leap year.'.format(year))
 else:
-    print("The year isn't a leap year!")
+  print('{} is not a leap year.'.format(year))
+
