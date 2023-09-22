7d892c2c7e91f042c02c42d1e0eaa1e7a2cca7c6
TUF008 <144257444+TUF008@users.noreply.github.com>
2023-09-22 06:56:08 +0530

    challenge 2.py

--- Challenge-Unit 1/challenge 2.py
+++ Challenge-Unit 1/challenge 2.py
@@ -1,6 +1,7 @@
 '''
 1.2 Write a program that determines whether a year entered by the user is a leap year or not using if-elif-else statements.
 '''
+
 def isLeapYear(year):
   if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
     return True
